bff710d14e310aa2d4cdf5fd59c628d717f99a92
priya8425 <yadavpriya1425@gmail.com>
2022-08-25 10:35:51 +0530

    b

--- action_itr.php
+++ action_itr.php
@@ -32,4 +32,36 @@ if(isset($_POST['packa'])){
     echo'</tbody>'
     ?>
 
+<?php
+if(isset($_POST['packa1'])){
+    $name=$_POST['packa1'];
+    $query=mysqli_query($conn,"SELECT * FROM `ITR_business` WHERE customer_id='$name'");
+    echo '  <thead>
+    <tr>
+      <th>Sr No.</th>
+      <th>Years</th>
+      <th>Total Income</th>
+      <th>TDS Refund</th>
+      <th>Tax Paid</th>
+      <th>Upload</th>
+    </tr>
+  </thead>
+  <tbody >
+';
+    $row=mysqli_fetch_array($query);
+    echo "
+    <tr>
+    <td>". $row['id'] ."</td>
+            <td>". $row['years'] ."</td>
+            <td>".$row['total_income']."</td>
+            <td>".$row['tds_refund'] ."</td>
+            <td>".$row['tax_paid'] ."</td>
+            <td>".$row['itr_upload'] ."</td>      
+    </tr> 
+    ";
+    }   
+    echo'</tbody>'
+    ?>
+
+
 
--- business-itr-reports.php
+++ business-itr-reports.php
@@ -122,17 +122,26 @@ include("include/config.php");
 
       <section class="content">
         <div class="container-fluid">
-          <div class="packageresult">
+          <div class="package1result">
 
             <div class="card">
                 <div class="col-12 my-3">
                   <div class="row text-center justify-content-center">
                     <label class="col-2">Select Business ITR User</label>
-                    <select class="form-control col-4" name="category" id="inputcategory">
-                      <option selected disabled>Select Business ITR User </option>
-                      <option>Individual</option>
-                      <option>Business</option>
-                    </select>
+                    <?php 
+                   $query=mysqli_query($conn,"select * from customer_registration");
+                   ?>
+                   <div class="col-4">
+                    <select  class="form-control"  onChange="package1(this.value)" name="category" id="inputcategory">
+                      <option selected disabled>Select ITR User</option>
+                      <?php
+                    while($sql=mysqli_fetch_array($query))
+                    {
+                      ?>
+                      <option value="<?php echo $sql['id']; ?>"> <?php echo $sql['name']; ?></option>
+                         <?php } ?>
+
+                         </select>
                   </div>
                   <div style="text-align:center; margin-top:inherit;">
                   <button class="btn btn-primary" style="float:middle;" type="submit" name="submit" id="submit" value="login">Submit</button>
@@ -150,17 +159,32 @@ include("include/config.php");
                   <div class="card-body">
                     <table id="example1" class="table table-bordered table-striped">
                       <thead>
-                        <tr>
+                      <tr>
                           <th>Sr No.</th>
-                          <th>Package</th>
-                          <th>Client Name</th>
-                          <th>Client Mobile No.</th>
-                          <th>Requirment</th>
-                          <th>Created On</th>
-                          <th>Action</th>
+                          <th>Years</th>
+                          <th>Total Income</th>
+                          <th>TDS Refund</th>
+                          <th>Tax Paid</th>
+                          <th>Upload</th>
                         </tr>
                       </thead>
-
+                      <tbody >
+                  
+                  <?php     
+                    $sql=mysqli_query($conn,"select * from ITR");
+                    $count=1;
+                    while($arr=mysqli_fetch_array($sql)){
+                    ?>
+                  <tr >
+                    <td><?php echo $count;?></td>
+                    <td><?php echo $arr['years'];?></td>
+                    <td><?php echo $arr['total_income'];?></td>
+                    <td><?php echo $arr['tds_refund'];?></td>
+                    <td><?php echo $arr['tax_paid'];?></td>
+                    <td><?php echo $arr['itr_upload'];?></td>                   
+                  </tr>                 
+                  <?php $count++; }  ?>
+                  </tbody>
                     </table>
                   </div>
                   <!-- /.card-body -->
@@ -186,78 +210,7 @@ include("include/config.php");
     <!-- /.control-sidebar -->
   </div>
   <!-- ./wrapper -->
-  <div class="modal fade closemaual" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
-    aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        </div>
-        <div class="modal-body">
-          <form method="post" action="">
-            <div class="row">
-              <div class="col-md-6">
-                <div class="form-group">
-                  <label>Package</label>
-                  <input type="hidden" value="<?php echo $id; ?>" id="firm_name" name="firm_name">
-                  <input type="text" value="" class="form-control" name="package" id="package">
-                </div>
-                <!-- /.form-group -->
-                <div class="form-group">
-                  <label>Client Mobile Number</label>
-                  <input type="tel" minlength="10" maxlength="10" onkeypress="return onlyNumberKey(event)"
-                    class="form-control" name="number" id="number" placeholder="Mobile Number" required>
-                  <span id="numberspan" class="mb-4"></span>
-                </div>
-                <!-- /.form-group -->
-              </div>
-              <!-- /.col -->
-              <div class="col-md-6">
-                <div class="form-group">
-                  <label>Client Name</label>
-                  <input type="text" class="form-control" name="Cname" id="cname" placeholder="Client Name" required>
-                  <span id="cnamespan" class="mb-4"></span>
-                </div>
-                <!-- /.form-group -->
-                <div class="form-group">
-                  <label>Requirement</label>
-                  <input type="text" class="form-control" name="requirement" id="Rname" placeholder="Requirement"
-                    required>
-                </div>
-                <!-- /.form-group -->
-              </div>
-              <!-- /.form-group -->
-            </div>
-            <div class="row">
-              <!-- /.col -->
-              <div class="col-md-6">
-                <div class="form-group">
-                  <label>Social Media</label>
-                  <select class="form-control select2" name="social_media" id="social_media" style="width: 100%;">
-                    <option selected="selected" disabled>Select</option>
-                    <option>Facebook</option>
-                    <option>Instagram</option>
-                    <option>Twitter</option>
-                    <option>Linkdin</option>
-                    <option>Youtube</option>
-                  </select>
-                </div>
-                <!-- /.form-group -->
-              </div>
-              <!-- /.form-group -->
-            </div>
-
-            <div class="modal-footer">
-              <button type="close" class="btn btn-default" data-dismiss="modal" name="close" id="close">Close</button>
-              <button type="button" name="submitt" class="btn btn-primary float-right my-3 " id="sub"
-                style="margin-right: 5px;">Submit </button>
-            </div>
-          </form>
-        </div>
-
-      </div>
-
-      <!-- Button trigger modal -->
+  
 
 
       <!-- jQuery -->
@@ -316,114 +269,19 @@ include("include/config.php");
 
         });
       </script>
-
-      <script>
-        function deleteBtn() {
-
-          swal({
-            title: "Are You Sure...?",
-            text: "This action can not be undone. Do you want to continue?",
-            icon: "warning",
-            buttons: ["No", "Yes"],
-          });
-
-          mobile_err = true;
-
-          mobile_check();
-
-          if (mobile_err = true) {
-            return true;
-          }
-          else { return false; }
-        }
-      </script>
-      <script>
-        $(document).ready(function () {
-          $('.delbtn').click(function (e) {
-            e.preventDefault();
-            let del_id = $(this).data('id');
-            swal({
-              title: "Are you sure?",
-              text: "Once deleted, you will not be able to recover this imaginary file!",
-              icon: "warning",
-              buttons: true,
-              dangerMode: true,
-            })
-              .then((willDelete) => {
-                if (willDelete) {
-                  swal("Poof! Your imaginary file has been deleted!", {
-                    icon: "success",
-                  });
-                  window.location.href = "view_clients.php?del_id" + del_id;
-                } else {
-                  swal("Your imaginary file is safe!");
-                }
-              });
-          })
-        });
-      </script>
-      <script>
-        function getdata(val) {
-          let fetch = $(".dropbtn").val();
-          let leadid = $("#leadid").val();
-          $.ajax({
-            url: "api_crm/action_client.php",
-            method: "POST",
-            data: {
-              fetch: fetch,
-              leadid: leadid
-            },
-            success: function (data) {
-              $('#leads').html(data);
-            }
-          });
-        }
-
-        function package(val) {
-          $("#package").val(val);
-          let firm_name = $("#leadid").val();
-          $.ajax({
-            url: "action_clients.php",
-            method: "POST",
-            data: {
-              packa: val,
-              firm_name: firm_name
-            },
-            success: function (data) {
-              $(".packageresult").html(data);
-            }
-          });
-        }
-
-        $(document).ready(function () {
-          $("#sub").click(function () {
-            let firm_name = $("#firm_name").val();
-            let package = $("#package").val();
-            let number = $("#number").val();
-            let cname = $("#cname").val();
-            let Rname = $("#Rname").val();
-            let social_media = $("#social_media").val();
-            let sub = $("#sub").val();
-            $.ajax({
-              type: "POST",
-              url: "action_clients.php",
-              data: {
-                firm_name: firm_name,
-                package: package,
-                number: number,
-                cname: cname,
-                Rname: Rname,
-                social_media: social_media,
-                sub: sub
-              },
-              success: function (data) {
-                $(".packresult").html(data);
-                $("#exampleModal").modal('hide');
-              }
-            });
-          });
-        });
-      </script>
+       <script>
+function package(val){
+    $.ajax({
+      url:"action_itr.php",
+      method:"POST",
+      data:{packa1:val},
+      success:function(data){
+        $("#example1").html(data);
+      }
+    
+    });
+  }
+  </script>
 </body>
 
 </html>
