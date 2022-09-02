6e067cb7c16f13a03bab5520c0b724a918e98467
priya8425 <yadavpriya1425@gmail.com>
2022-09-02 16:37:18 +0530

    c

--- business_itr.php
+++ business_itr.php
@@ -84,6 +84,21 @@ if(isset($_POST['submit']))
 
     <!-- Content Wrapper. Contains page content -->
     <div class="content-wrapper">
+    <section class="content-header">
+        <div class="container-fluid">
+          <div class="row mb-2">
+            <div class="col-sm-6">
+              <h1>Business ITR</h1>
+            </div>
+            <div class="col-sm-6">
+              <ol class="breadcrumb float-sm-right">
+                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                <li class="breadcrumb-item active">Business ITRs</li>
+              </ol>
+            </div>
+          </div>
+        </div><!-- /.container-fluid -->
+      </section>
       <section class="content-header">
         <div class="container-fluid">
 
--- gst_filling.php
+++ gst_filling.php
@@ -96,6 +96,21 @@ th {
 
     <!-- Content Wrapper. Contains page content -->
     <div class="content-wrapper">
+    <section class="content-header">
+        <div class="container-fluid">
+          <div class="row mb-2">
+            <div class="col-sm-6">
+              <h1>GST</h1>
+            </div>
+            <div class="col-sm-6">
+              <ol class="breadcrumb float-sm-right">
+                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                <li class="breadcrumb-item active">GST</li>
+              </ol>
+            </div>
+          </div>
+        </div><!-- /.container-fluid -->
+      </section>
       <section class="content-header">
         <div class="container-fluid">
 
